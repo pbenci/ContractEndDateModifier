2b914ac3f0b59bca6ae916c4be0202fdad638982
paobenci@gmail.com <paobenci@gmail.com>
2023-01-25 17:43:50 +0100

    Add project files.

---
+++ ContractEndDateModifier/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContractEndDateModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
