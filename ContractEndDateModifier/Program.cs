6344aed7fcf0597ca2760a7aac78f60641c617a0
paobenci@gmail.com <paobenci@gmail.com>
2023-01-25 18:01:15 +0100

    Added program basic logic

--- ContractEndDateModifier/Program.cs
+++ ContractEndDateModifier/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ContractEndDateModifier
+﻿using OpenQA.Selenium;
+using System.Configuration;
+
+namespace ContractEndDateModifier
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Parallel.ForEach(Excel.GetNumberOfUsedRows(), new ParallelOptions { MaxDegreeOfParallelism = int.Parse(ConfigurationManager.AppSettings.Get("ParallelisationLevel")) }, Contract =>
+            {
+                IWebDriver Driver = new Browsers().LaunchChrome();
+                Driver.Manage().Window.Maximize();
+                try
+                {
+                    var LoginPage = new LoginPage(Driver);
+                    LoginPage.GoToUrl();
+                    LoginPage.Login();
+                    var RentalsPage = new RentalsPage(Driver);
+                    RentalsPage.GoTo();
+                    RentalsPage.SearchContractByCode(Contract.Code);
+                    var RentalsDetailsPage = new RentalsDetailsPage(Driver, Contract.ContractRowId);
+                    RentalsDetailsPage.GoTo();
+                    RentalsDetailsPage.OpenEditContractMode();
+                    //TODO EDIT DATE 1 DAY FORWARD AND SAVE CONTRACT MODIFICATION
+                    //TODO EDIT DATE 1 DAY BACK AND SAVE CONTRACT MODIFICATION
+                    Driver.Quit();
+                }
+                catch (Exception)
+                {
+                    Driver.Quit();
+                }
+            });
         }
     }
 }
