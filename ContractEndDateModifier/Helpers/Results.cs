79dcb2b77e7f7624d0b75a53c139018e7d144a88
paobenci@gmail.com <paobenci@gmail.com>
2023-04-27 08:29:46 +0200

    Added logging errors class

---
+++ ContractEndDateModifier/Helpers/Results.cs
@@ -0,0 +1,10 @@
+﻿namespace ContractEndDateModifier
+{
+    public static class Results
+    {
+        public static void Log(string Text)
+        {
+            File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + @"\Results.txt", DateTime.Now + " - " + Text + Environment.NewLine);
+        }
+    }
+}
